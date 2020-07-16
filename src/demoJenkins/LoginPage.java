620214c3afd0f9d8e5f1c8b3fdee6019b269b58f
arvindbangar <arvind.bangar@worldline.com>
2020-07-16 20:21:08 +0530

    Added Search Page & Login Page Feature code

---
+++ src/demoJenkins/LoginPage.java
@@ -0,0 +1,10 @@
+package demoJenkins;
+
+public class LoginPage {
+
+	public void loginPage() {
+		System.out.println("login Page");
+	}
+	
+	
+}
---
+++ src/demoJenkins/SearchPage.java
@@ -0,0 +1,10 @@
+package demoJenkins;
+
+public class SearchPage {
+	
+	public void searchPage() {
+		System.out.println("Search Page");
+	}
+	
+	
+}
